91270923619cd90b6e7db5729de3bb1baa023c62
SlozilTomas <sloziltomas1@gmail.com>
2024-10-10 08:22:59 +0200

    Update first.py
    uprava

--- first.py
+++ first.py
@@ -1,11 +1,11 @@
 def hello():
     print("Hello World!")
 
-def sudy_lichy(cislo):
+def sudy_nebo_lichy(cislo):
     if (cislo%2==1):
         print(f"Cislo {cislo} je liche.")
     else:
         print(f"Cislo {cislo} je sude.")
 
-sudy_lichy(5)
-sudy_lichy(1000000)
+sudy_nebo_lichy(5)
+sudy_nebo_lichy(1000000)
